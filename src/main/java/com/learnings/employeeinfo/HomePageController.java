5f47a2b835c4616b68bdc422c10febc9284b6115
kiranmai <kiranmaipentela@gmail.com>
2021-05-05 16:53:37 +0530

    addded missed files

--- src/main/java/com/learnings/employeeinfo/HomePageController.java
+++ src/main/java/com/learnings/employeeinfo/HomePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomePageController  {
 
     @RequestMapping(value = "/")
-    public String index() {
+    public String homePage() {
         return "index";
     }
 }
